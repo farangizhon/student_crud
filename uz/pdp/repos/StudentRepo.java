fc1afcf8fbb87182d21fc7a46fdf458cbd833f02
farangiz <farangizhon2004@icloud.com>
2024-01-15 21:08:35 +0500

    completed the findAll method

--- uz/pdp/repos/StudentRepo.java
+++ uz/pdp/repos/StudentRepo.java
@@ -51,7 +51,7 @@ public class StudentRepo implements Repository<Student> {
 
     @Override
     public List<Student> findAll() {
-        return null;
+        return students;
     }
 
     @Override
