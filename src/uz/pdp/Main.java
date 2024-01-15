f62978568eef0ba71526ff0e31805301ed8df89c
farangiz <farangizhon2004@icloud.com>
2024-01-15 22:29:54 +0500

    printed

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -1,8 +1,14 @@
 package uz.pdp;
 
+import uz.pdp.services.StudentService;
+import uz.pdp.utils.Input;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome");
+        while (true) {
+            StudentService.printAll();
+        }
     }
 }
