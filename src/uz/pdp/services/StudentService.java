1d7607d9d43c9753d066fa62a1fef678b25959be
farangiz <farangizhon2004@icloud.com>
2024-01-15 22:02:33 +0500

    wrote the add method (the part when a new student is created)

--- src/uz/pdp/services/StudentService.java
+++ src/uz/pdp/services/StudentService.java
@@ -2,6 +2,7 @@ package uz.pdp.services;
 
 import uz.pdp.entity.Student;
 import uz.pdp.repos.StudentRepo;
+import uz.pdp.utils.Input;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +17,11 @@ public class StudentService {
             }
         }
         return Optional.empty();
+    }
+    public static void add() {
+        Student student = new Student(
+                Input.inputStr("Enter name"),
+                Input.inputInt("Enter age")
+        );
     }
 }
