e8893701bbc5052816aacd2b43cfb169d7762631
farangiz <farangizhon2004@icloud.com>
2024-01-15 19:31:02 +0500

    created a toString for Student

--- Student.java
+++ Student.java
@@ -36,4 +36,9 @@ public class Student {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + age;
+    }
 }
