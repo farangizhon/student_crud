545f4f3f9723c4bc47756234192f690a2375f3ee
farangiz <farangizhon2004@icloud.com>
2024-01-15 19:20:13 +0500

    created getters and setters for Student

--- Student.java
+++ Student.java
@@ -2,4 +2,33 @@ public class Student {
     private final int id = counter++;
     private static int counter = 1;
     private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void setCounter(int counter) {
+        Student.counter = counter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
