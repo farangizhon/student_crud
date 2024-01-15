36e9a5027f5f870dbf71373637e8813fcfbd0f41
farangiz <farangizhon2004@icloud.com>
2024-01-15 20:48:38 +0500

    created student list and implemented Repository as well as overriding the methods

--- uz/pdp/repos/StudentRepo.java
+++ uz/pdp/repos/StudentRepo.java
@@ -1,4 +1,30 @@
 package uz.pdp.repos;
 
-public class StudentRepo {
+import uz.pdp.db.Repository;
+import uz.pdp.entity.Student;
+
+import java.util.List;
+
+public class StudentRepo implements Repository<Student> {
+    List<Student> students;
+
+    @Override
+    public void save(Student student) {
+
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Student student, Integer id) {
+
+    }
+
+    @Override
+    public void delete(Student student) {
+
+    }
 }
